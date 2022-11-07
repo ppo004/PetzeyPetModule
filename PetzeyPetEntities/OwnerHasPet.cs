1054c495aa351e6d383b3907cda99158b7a54551
ppo004 <prajwalshiv.04@gmail.com>
2022-11-07 17:05:55 +0530

    Added 2 more entities

---
+++ PetzeyPetEntities/OwnerHasPet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetzeyPetEntities
+{
+    public class OwnerHasPet
+    {
+        [Key]
+        public int OwnerPetId { get; set; }
+        public int PetId { get; set; }
+    }
+}
---
+++ PetzeyPetEntities/PetAndAppointments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetzeyPetEntities
+{
+    public class PetAndAppointments
+    {
+        [Key]
+        public int PetAppId { get; set; }
+        [Required]
+        public int AppointmentId { get; set; }
+
+    }
+}
