099e696b851f7904f94568417f4cc7709c935d37
SBFaisal <faisalsb0786@gmail.com>
2022-11-08 12:37:56 +0530

    Added PetOwner Controller

--- PetzeyPetDataAccessLayer/PetOwnerRepository/PetOwnerRepository.cs
+++ PetzeyPetDataAccessLayer/PetOwnerRepository/PetOwnerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetzeyPetEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,35 @@ using System.Threading.Tasks;
 
 namespace PetzeyPetDataAccessLayer.PetOwnerRepository
 {
-    public class PetOwnerRepository
+    public class PetOwnerRepository: IPetOwnerRepository
     {
+        PetDbContext db = new PetDbContext();
+
+        public void CreateOwner(PetOwner petOwner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddProfilePic(int petOwnerId, string imageUrl)
+        {
+            var owner = db.PetOwners.Find(petOwnerId);
+            owner.ImageUrl = imageUrl;
+        }
+
+        public void DeleteProfilePic(int petOwnerId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void EditOwner(PetOwner petOwner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetOwnerNames()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
