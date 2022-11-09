1aab388052a5d9e19140b9e3d9aeeab046d91111
ppo004 <prajwalshiv.04@gmail.com>
2022-11-09 12:09:16 +0530

    Added swashbuckle

--- PetzeyPetApi/App_Start/UnityConfig.cs
+++ PetzeyPetApi/App_Start/UnityConfig.cs
@@ -41,7 +41,7 @@ namespace PetzeyPetApi
             // container.LoadConfiguration();
 
             // TODO: Register your type's mappings here.
-            // container.RegisterType<IProductRepository, ProductRepository>();
+            //container.RegisterType<IPet, ProductRepository>();
         }
     }
 }
